1ff2cb260f986d39697796a3ef861b8bc755e0ef
fasometer <fasometer@yandex.ru>
2025-06-04 23:22:04 +0300

    SQL_04_06

--- main.py
+++ main.py
@@ -6148,21 +6148,54 @@
 #
 # print(msg)
 
+# import sqlite3
+#
+# # con = sqlite3.connect("profile.db")
+# # cur = con.cursor()
+# #
+# # cur.execute("")
+# #
+# # con.close()
+#
+# with  sqlite3.connect("profile.db") as con:
+#     cur = con.cursor()
+#     cur.execute("""CREATE TABLE IF NOT EXISTS users(
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     name TEXT NOT NULL,
+#     summa REAL,
+#     data TEXT
+#     )""")
+#     cur.execute("DROP TABLE users")
+
 import sqlite3
 
-# con = sqlite3.connect("profile.db")
-# cur = con.cursor()
-#
-# cur.execute("")
-#
-# con.close()
-
-with  sqlite3.connect("profile.db") as con:
+with  sqlite3.connect("users.db") as con:
     cur = con.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS users(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    summa REAL,
-    data TEXT
-    )""")
-    cur.execute("DROP TABLE users")
+    # cur.execute("""CREATE TABLE IF NOT EXISTS users(
+    # id INTEGER PRIMARY KEY AUTOINCREMENT,
+    # name TEXT NOT NULL,
+    # phone BLOB NOT NULL DEFAULT "+7 909 1234567",
+    # age INTEGER CHECK(age > 0 AND age < 100),
+    # email TEXT UNIQUE
+    #
+    # )""")
+# переименование таблицы
+#     cur.execute("""
+#     ALTER TABLE users
+#     RENAME TO person_table;
+#     """)
+# добавление новго столбца
+#     cur.execute("""
+#     ALTER TABLE person_table
+#     ADD COLUMN address TEXT ;
+#     """)
+#     # ПЕРИМНОВАТЬСТОЛБЕЦ
+#     cur.execute("""
+#     ALTER TABLE person_table
+#     RENAME COLUMN address TO  home_address ;
+#     """)
+#     # удаление столбца
+#     cur.execute("""
+#     ALTER TABLE person_table
+#     DROP COLUMN home_address;
+#     """)
