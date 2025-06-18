49e3764e4749e0d8380f882c37bbf6c324e6b9f0
fasometer <fasometer@yandex.ru>
2025-06-18 16:39:28 +0300

    17_06_work

--- DZ/dz_one/dz_flask.py
+++ DZ/dz_one/dz_flask.py
@@ -1,3 +1,24 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+menu = [
+    {'name': "Главная", "url": "index"},
+    {'name': "Наши проекты", "url": "project"},
+    {'name': "Связаться с нами", "url": "contact"}
+]
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template('index.html', title='Главная', menu=menu)
+
+
+@app.route("/project")
+def project():
+    return render_template('project.html', title='Наши проекты', menu=menu)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
