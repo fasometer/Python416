1c03f7eb0aa2de825a50b7e1d5f6cda82a265c36
fasometer <fasometer@yandex.ru>
2025-07-02 22:40:40 +0300

    django_02_07

---
+++ second/personal_portfolio/skills/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+# Create your models here.
+class Skills(models.Model):
+    title = models.CharField(max_length=100)  # тип данных и ограничение на ввод символов
+    description = models.CharField(max_length=250)
+    image = models.ImageField('skills/images/')
+    url = models.URLField(blank=True)
