50583eb4064c0189177a6517ed86facc5194ab54
fasometer <fasometer@yandex.ru>
2025-08-11 00:03:02 +0300

    dz_11_08

--- DZ/find_solutions/tasks/forms.py
+++ DZ/find_solutions/tasks/forms.py
@@ -1,8 +1,18 @@
 from django.forms import ModelForm
 from .models import Task
+from django import forms
+
+f = (
+    ('1', 'PK1'),
+    ('2', 'PK2'),
+    ('3', 'PK3'),
+
+)
 
 
 class TaskForm(ModelForm):
+    liens = forms.ChoiceField(choices=f)
+
     class Meta:
         model = Task
-        fields = ['title', 'memo', 'decision', 'decision_images', 'important']
+        fields = ['title', 'memo', 'memo_images', 'decision', 'decision_images', 'important','line']
--- DZ/find_solutions/tasks/models.py
+++ DZ/find_solutions/tasks/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # Create your models here.
+f = (
+    ('1', 'PK1'),
+    ('2', 'PK2'),
+    ('3', 'PK3'),
+
+)
+
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
@@ -10,8 +16,9 @@ class Task(models.Model):
     memo_images = models.ImageField(upload_to="memo/%Y/%m/%d/", blank=True)
     decision = models.TextField(blank=True)
     decision_images = models.ImageField(upload_to="decision/%Y/%m/%d/", blank=True)
-    created = models.DateTimeField(auto_now_add=True)
-    data_complete = models.DateTimeField(blank=True, null=True)
+    created = models.DateField(auto_now_add=True)
+    data_complete = models.DateField(blank=True, null=True)
+    line = models.CharField(max_length=100, choices=f,default='PK1')
     important = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
