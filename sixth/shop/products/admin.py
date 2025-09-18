6262d46d71575d6e281a104ae29474e9f6739f82
fasometer <fasometer@yandex.ru>
2025-09-18 21:46:36 +0300

    118_09

--- sixth/shop/products/admin.py
+++ sixth/shop/products/admin.py
@@ -14,6 +14,7 @@ class CsvImportForm(forms.Form):
 # Register your models here.
 
 class ProductCategoryAdmin(admin.ModelAdmin):
+
     def get_urls(self):
         urls = super().get_urls()
         new_urls = [path("upload-csv/", self.upload_csv)]
@@ -24,7 +25,7 @@ class ProductCategoryAdmin(admin.ModelAdmin):
             csv_file = request.FILES['csv_uploader']
 
             if not csv_file.name.endswith('.csv'):
-                messages.warning(request,"Ошибочный тип файла")
+                messages.warning(request, "Ошибочный тип файла")
                 return redirect('.')
 
             file_data = csv_file.read().decode("utf-8")
@@ -45,6 +46,88 @@ class ProductCategoryAdmin(admin.ModelAdmin):
         return render(request, "admin/csv_uploader.html", data)
 
 
+class PhotoAdd(admin.StackedInline):  # из одной модели в другую
+    model = Photo
+    fields = ('product', 'add_photo')
+    extra = 0   # убрать лишние строки
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'price', 'quantity')
+    list_display_links = ('id', 'name')
+    inlines = [PhotoAdd]
+
+    def get_urls(self):
+        urls = super().get_urls()
+        new_urls = [path("upload-csv-product/", self.upload_csv)]
+        return new_urls + urls
+
+    def upload_csv(self, request):
+        if request.method == "POST":
+            csv_file = request.FILES['csv_uploader']
+
+            if not csv_file.name.endswith('.csv'):
+                messages.warning(request, "Ошибочный тип файла")
+                return redirect('.')
+
+            file_data = csv_file.read().decode("utf-8")
+            csv_data = file_data.split("\n")
+
+            for x in csv_data:
+                fields = x.split(",")
+                # print(fields)
+                Product.objects.update_or_create(
+                    id=fields[0],
+                    name=fields[1],
+                    image=fields[2],
+                    description=fields[3],
+                    short_description=fields[4],
+                    price=fields[5],
+                    quantity=fields[6],
+                    category=ProductCategory(fields[7][0])  # связь
+                )
+            return redirect('admin:index')
+
+        form = CsvImportForm()
+        data = {"form": form}
+        return render(request, "admin/csv_uploader.html", data)
+
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('product', 'add_photo')
+
+    def get_urls(self):
+        urls = super().get_urls()
+        new_urls = [path("upload-csv-photo/", self.upload_csv)]
+        return new_urls + urls
+
+    def upload_csv(self, request):
+        if request.method == "POST":
+            csv_file = request.FILES['csv_uploader']
+
+            if not csv_file.name.endswith('.csv'):
+                messages.warning(request, "Ошибочный тип файла")
+                return redirect('.')
+
+            file_data = csv_file.read().decode("utf-8")
+            csv_data = file_data.split("\n")
+
+            for x in csv_data:
+                fields = x.split(",")
+                # print(fields)
+                Photo.objects.update_or_create(
+                    id=fields[0],
+                    product=Product(fields[1]),
+                    add_photo=fields[2][:-1]
+                )
+
+            return redirect('admin:index')
+
+        form = CsvImportForm()
+        data = {"form": form}
+        return render(request, "admin/csv_uploader.html", data)
+
+
 admin.site.register(ProductCategory, ProductCategoryAdmin)
-admin.site.register(Product)
-admin.site.register(Photo)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Photo, PhotoAdmin)
--- sixth/shop/products/urls.py
+++ sixth/shop/products/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('products/', views.products, name='products'),
+    path('products/<int:category_id>', views.products, name='category'),
+    path('product/<int:pk>', views.product, name='product'),
+    path('products/page/<int:page>', views.products, name='page'),
+
 ]
